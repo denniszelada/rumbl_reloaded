c5fe21ee23fa643c7108a6c9f5af14dec731c64f
Dennis Zelada <denniszelada@gmail.com>
2020-01-23 14:05:07 +0100

    Add handler to not show duplications of annotations when we get disconnected

--- config/dev.exs
+++ config/dev.exs
@@ -2,8 +2,8 @@ use Mix.Config
 
 # Configure your database
 config :rumbl, Rumbl.Repo,
-  username: "postgres",
-  password: "",
+  username: "elixir",
+  password: "kyks_dev",
   database: "rumbl_dev",
   hostname: "localhost",
   show_sensitive_data_on_connection_error: true,
